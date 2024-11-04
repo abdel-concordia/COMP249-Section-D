8a6d05816b71f2de32e452340726777c966a9a9e
AB <abdel@MSSPro.wireless.concordia.ca>
2024-11-04 15:53:05 -0500

    More examples, Recursion

--- Comp249-Section-D/src/comp249_section_d/MainClass.java
+++ Comp249-Section-D/src/comp249_section_d/MainClass.java
@@ -4,7 +4,9 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        System.out.println(sumUpTo(4));
+        int[] array = {1, -5, -10, 7};
+
+        System.out.println(sumPositive(array, 0));
 
     }
 
@@ -40,9 +42,49 @@ public class MainClass {
         }
     }
 
-    public static int arraySum(int[] array) {
+    public static int arraySum(int[] array, int index) {
         // Base case
+        if (index == array.length - 1) // Recursive call
+        {
+            return array[index];
+        } else {
+            return array[index] + arraySum(array, index + 1);
+        }
+    }
 
-        // Recursive call
+    public static int positiveCount(int[] array, int index) {
+        // Base a case
+        if (index == 0) {
+            if (array[index] >= 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            // Recursive call
+            if (array[index] >= 0) {
+                return 1 + positiveCount(array, index - 1);
+            } else {
+                return positiveCount(array, index - 1);
+            }
+        }
+    }
+
+    public static int sumPositive(int[] array, int index) {
+        // Base case
+        if (index == array.length - 1) {
+            if (array[index] >= 0) {
+                return array[index];
+            } else {
+                return 0;
+            }
+        } else {
+            // Recursive call
+            if (array[index] >= 0) {
+                return array[index] + sumPositive(array, index + 1);
+            } else {
+                return sumPositive(array, index + 1);
+            }
+        }
     }
 }
