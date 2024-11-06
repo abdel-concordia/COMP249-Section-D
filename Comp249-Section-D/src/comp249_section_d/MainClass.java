fd53be9342387dcd802e6ddb0d306cda21b5be3d
AB <abdel@MSSPro.wireless.concordia.ca>
2024-11-06 15:55:14 -0500

    First Comparable interface

--- Comp249-Section-D/src/comp249_section_d/MainClass.java
+++ Comp249-Section-D/src/comp249_section_d/MainClass.java
@@ -4,87 +4,14 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        int[] array = {1, -5, -10, 7};
+        Student2 s1 = new Student2(3.4);
+        Student2 s2 = new Student2(4.0);
 
-        System.out.println(sumPositive(array, 0));
+        s1.equals(s2); // gives true
 
+        String str = new String("Hello");
+
+        s1.compareTo(str); // 0
     }
 
-    public static void countDown(int n) {
-        // Base case
-        if (n == 0) {
-            System.out.println(0);
-        } // Recursive call
-        else {
-            System.out.println(n);
-            countDown(n - 1);
-        }
-    }
-
-    public static void countUp(int n) {
-        // Base case
-        if (n == 0) {
-            System.out.println(0);
-        } // Recursive call
-        else {
-            countUp(n - 1);
-            System.out.println(n); //
-        }
-    }
-
-    public static int sumUpTo(int n) {
-        // Base case
-        if (n == 0) {
-            return 0;
-        } else {
-            // Recursive call
-            return n + sumUpTo(n - 1);
-        }
-    }
-
-    public static int arraySum(int[] array, int index) {
-        // Base case
-        if (index == array.length - 1) // Recursive call
-        {
-            return array[index];
-        } else {
-            return array[index] + arraySum(array, index + 1);
-        }
-    }
-
-    public static int positiveCount(int[] array, int index) {
-        // Base a case
-        if (index == 0) {
-            if (array[index] >= 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        } else {
-            // Recursive call
-            if (array[index] >= 0) {
-                return 1 + positiveCount(array, index - 1);
-            } else {
-                return positiveCount(array, index - 1);
-            }
-        }
-    }
-
-    public static int sumPositive(int[] array, int index) {
-        // Base case
-        if (index == array.length - 1) {
-            if (array[index] >= 0) {
-                return array[index];
-            } else {
-                return 0;
-            }
-        } else {
-            // Recursive call
-            if (array[index] >= 0) {
-                return array[index] + sumPositive(array, index + 1);
-            } else {
-                return sumPositive(array, index + 1);
-            }
-        }
-    }
 }
---
+++ Comp249-Section-D/src/comp249_section_d/Student2.java
@@ -0,0 +1,32 @@
+package comp249_section_d;
+
+public class Student2 implements Comparable {
+
+    private double gpa;
+
+    public Student2(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public int compareTo(Object otherObject) {
+
+        Student2 otherStudent = (Student2) otherObject;
+
+        if (this.gpa < otherStudent.gpa) {
+            return -1;
+        } else if (this.gpa > otherStudent.gpa) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
