1cea5cf02cdcc16cd9dd34dfbb2336441982b06c
AB <abdel@MSSPro.wireless.concordia.ca>
2024-11-13 15:13:37 -0500

    ArrayList<String> example

--- Comp249-Section-D/src/comp249_section_d/MainClass.java
+++ Comp249-Section-D/src/comp249_section_d/MainClass.java
@@ -6,10 +6,20 @@ public class MainClass {
 
     public static void main(String[] args) { // Static context
 
-        ArrayList<String> names = new ArrayList<>(5);
+        ArrayList<String> names = new ArrayList<>(20); // int: primitive type | Integer: a class that can hold int value
 
-        names.add("John");
+        names.add("John"); // add at the end
+        names.add("Jane");
+        names.add("Ali");
 
+        names.add(1, "Michel"); // adds at specified position
+
+        //names.remove("Ali");
+        System.out.println(names);
+
+//        for (int i = 0; i < names.size(); i++) {
+//            System.out.println(names.get(i));
+//        }
     }
 
 }
