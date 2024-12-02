7d22659abfdbd48a510ee3e1555dbb3e065b50d7
AB <abdel@MSSPro.wireless.concordia.ca>
2024-12-02 15:50:05 -0500

    Comparators example

--- Comp249-Section-D/src/comp249_section_d/MainClass.java
+++ Comp249-Section-D/src/comp249_section_d/MainClass.java
@@ -1,24 +1,98 @@
 package comp249_section_d;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainClass {
 
     public static void main(String[] args) { // Static context
 
-        CircularLinkedList1<Integer> iList = new CircularLinkedList1();
+        ArrayList<A> myList = new ArrayList<>();
 
-        iList.addAtHead(30);
+        myList.add(new A());
+        myList.add(new A());
+        myList.add(new A());
 
-        iList.addAtHead(20);
-        iList.addAtHead(10);
-        iList.addAtHead(5);
-        iList.displayElements();
+        Collections.sort(myList, new ComparatorBasedOnNumber1());
 
-        CircularLinkedList1<String> sList = new CircularLinkedList1<>();
+        // Use the list
+        Collections.sort(myList, new ComparatorBasedOnNumber2());
 
-        sList.addAtHead("Hi");
+    }
 
+}
+
+class A {
+
+    private int number1;
+    private int number2;
+    private int number3;
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public int getNumber3() {
+        return number3;
+    }
+
+    public void setNumber3(int number3) {
+        this.number3 = number3;
+    }
+
+}
+
+class ComparatorBasedOnNumber1 implements Comparator<A> {
+
+    public int compare(A a1, A a2) {
+        if (a1.getNumber1() < a2.getNumber1()) {
+            return -1;
+        } else if (a1.getNumber1() == a2.getNumber1()) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+}
+
+class ComparatorBasedOnNumber2 implements Comparator<A> {
+
+    public int compare(A a1, A a2) {
+        if (a1.getNumber2() < a2.getNumber2()) {
+            return -1;
+        } else if (a1.getNumber2() == a2.getNumber2()) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+}
+
+class ComparatorBasedOnNumberDescending implements Comparator<A> {
+
+    public int compare(A a2, A a1) {
+        if (a1.getNumber3() < a2.getNumber3()) {
+            return -1;
+        } else if (a1.getNumber2() == a2.getNumber2()) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 
 }
